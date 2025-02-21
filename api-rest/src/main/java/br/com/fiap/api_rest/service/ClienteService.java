19ed284f04b2c7c4e2825f9837273403b8064abe
Murilo Capristo <161600355+Murilo-Capristo@users.noreply.github.com>
2025-02-21 12:00:47 -0300

    sei la fiz mais coisa

--- api-rest/src/main/java/br/com/fiap/api_rest/service/ClienteService.java
+++ api-rest/src/main/java/br/com/fiap/api_rest/service/ClienteService.java
@@ -3,13 +3,22 @@ package br.com.fiap.api_rest.service;
 import br.com.fiap.api_rest.dto.ClienteRequest;
 import br.com.fiap.api_rest.dto.ClienteResponse;
 import br.com.fiap.api_rest.model.Cliente;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteService {
 
     public Cliente requestToCliente(ClienteRequest clienteRequest) {
-        return new Cliente(null, clienteRequest.getNome(), clienteRequest.getIdade());
+
+        return new Cliente(null,
+                clienteRequest.getNome(),
+                clienteRequest.getIdade(),
+                clienteRequest.getEmail(),
+                clienteRequest.getSenha(),
+                clienteRequest.getCpf(),
+                clienteRequest.getCategoria());
 
     }
 
