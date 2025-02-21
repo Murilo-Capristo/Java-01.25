ec175254b336888b3ae76d6bec8cb971ce1c2a70
Murilo Capristo <161600355+Murilo-Capristo@users.noreply.github.com>
2025-02-21 11:37:07 -0300

    Regex + Annotation

--- api-rest/src/main/java/br/com/fiap/api_rest/dto/ClienteRequest.java
+++ api-rest/src/main/java/br/com/fiap/api_rest/dto/ClienteRequest.java
@@ -1,30 +1,50 @@
 package br.com.fiap.api_rest.dto;
 
+import br.com.fiap.api_rest.enums.Categoria;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.hibernate.validator.constraints.br.CPF;
+
+@Data
+@AllArgsConstructor
 public class ClienteRequest {
+    @NotBlank(
+            message= "O nome não pode ser em branco!"
+    )
+    @Size(  min = 3,
+            max = 150,
+            message = "O nome deve ter entre 3 e 150 caracteres"
+    )
     private String nome;
+    //////////////////////////////////////////////////
+    @Min(
+            value=18,
+            message = ("O cliente deve ter no mínimo 18 anos")
+    )
     private int idade;
+    //////////////////////////////////////////////////
+    @Email(
+            message = "Email fora do formato correto"
+    )
+    private String email;
+    //////////////////////////////////////////////////
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[@!,#])(?=.*\\d)[A-Za-z\\d\\W].{8,}$",
+            message = "A senha deve ter pelo menos 8 caracteres, incluindo uma letra maiúscula, uma minúscula e um caractere especial."
+    )
 
-    public ClienteRequest(String nome, int idade) {
-        this.nome = nome;
-        this.idade = idade;
-    }
+    private String senha;
+    /////////////////////////////////////////////////
+    @CPF(
+            message = "Cpf Inválido"
+    )
+    private String cpf;
+    ////////////////////////////////////////////////////
+    @NotNull(
+            message = "A categoria é obrigatória!"
+    )
+    private Categoria categoria;
+    //////////////////////////////////////////////////
 
-    public ClienteRequest() {
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
--- api-rest/src/main/java/br/com/fiap/api_rest/model/Cliente.java
+++ api-rest/src/main/java/br/com/fiap/api_rest/model/Cliente.java
@@ -1,50 +1,26 @@
 package br.com.fiap.api_rest.model;
 
+import br.com.fiap.api_rest.enums.Categoria;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @Entity
+@Data
+@AllArgsConstructor
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private int idade;
+    private String email;
+    private String senha;
+    private String cpf;
+    private Categoria categoria;
 
-
-    public Cliente() {
-    }
-
-    public Cliente(Long id, String nome, int idade) {
-        this.id = id;
-        this.nome = nome;
-        this.idade = idade;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
 
